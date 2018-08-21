a44ece5e67519984ad4da5eab6ad12d24e1bdf76
Matheus Assis <30660987+MatheusMAssis@users.noreply.github.com>
2018-08-21 12:04:39 -0300

    Update word-Recommendation.py

--- word-Recommendation.py
+++ word-Recommendation.py
@@ -1,35 +1,7 @@
-import time as t
-
-#--- ---#
-    
-def different_letters(str1, str2, keyboard):  #--- keyboard is used just to have a alphabet ---#
-    ans = {}
-    res = []
-    for line in keyboard:
-        for letter in line:
-            if letter != None:
-                ans[letter] = 0
-                
-    for letter in str1:
-        ans[letter] = 1
-        
-    for letter in str2:
-        if ans[letter] == 1:
-            ans[letter] = -1
-        else:
-            ans[letter] = 2
-    
-    for letter in ans:
-        if ans[letter] != 0 and ans[letter] != -1:
-            res.append(letter)
-    
-    return res
-
-
 #--- Word Recommendation using Levenshtein Distance ---#
 
 
-def recommendation(string):
+def recommendation(string, keyboard):
     start = t.time()
     
     #--- reading txt common_words_usa.txt (with 10000 words) ---#
@@ -44,8 +16,15 @@ def recommendation(string):
     
     ans = [] 
     for word in data:
-        if len(string) - len(word) < 2: #--- we optimize by avoid doing Levenshtein for all string ---#
+        if len(string) - len(word) < 2:                 #--- we optimize by avoid doing Levenshtein for all string ---#
             value = levenshtein(string, word)
-            if value < 2 and value != 0: #--- it cannot recommend the string itself ---#
-                ans.append(word)
+            if value < 2 and value != 0:                #--- it cannot recommend the string itself ---#
+                if len(string) == len(word):            #--- if they have the same lenght, we will see the distance between different letters on the keyboard ---#
+                    for i in range(len(string)):
+                        if string[i] != word[i]:
+                            if keyboard_dist(keyboard, string[i], word[i]) < 2:
+                                ans.append(word)
+                else:                                   #--- if they have different lenghts, it is expected to be a recommendation ---#
+                    ans.append(word)
+                    
     return ans, t.time() - start
